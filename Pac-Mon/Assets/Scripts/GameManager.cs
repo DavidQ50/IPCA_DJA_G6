7204c80507919a738e580ed0b4cb1e8591d7c86f
Zemina02 <156537921+Zemina02@users.noreply.github.com>
2026-05-12 17:53:16 +0100

    script para as esferas e adição do modelo do pacmon

--- Pac-Mon/Assets/Scripts/GameManager.cs
+++ Pac-Mon/Assets/Scripts/GameManager.cs
@@ -2,17 +2,42 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
+    int pontuacaoModoInfinito;
+    GameObject[] spheres;
     public static GameManager Instance;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
+    void sphereCount()
+    {
+        spheres = GameObject.FindGameObjectsWithTag("Spheres");
+        Debug.Log("Number of spheres: " + spheres.Length);
+    }
+
+    void openDoor()
+    {
+        int sphereCount = spheres.Length;
+
+        if (sphereCount == 0) // Example condition, adjust as needed
+        {
+            Debug.Log("All spheres collected! Door opened.");
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        sphereCount();
+        openDoor();
     }
 }
