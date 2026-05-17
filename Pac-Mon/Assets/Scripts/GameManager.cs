3841e64a8a696e4cffc768fcacf5c40df582c2ca
DavidQ50 <davidquerido06@gmail.com>
2026-05-17 20:51:18 +0100

    portal

--- Pac-Mon/Assets/Scripts/GameManager.cs
+++ Pac-Mon/Assets/Scripts/GameManager.cs
@@ -1,53 +1,32 @@
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+namespace game
 {
-    int pontuacaoModoInfinito;
-    GameObject[] spheres;
-    public static GameManager Instance;
-    public Animator portalAnimator;
-
-    private void Awake()
+    public class GameManager : MonoBehaviour
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    void sphereCount()
-    {
-        spheres = GameObject.FindGameObjectsWithTag("Spheres");
-        Debug.Log("Number of spheres: " + spheres.Length);
-    }
-
-    void openDoor()
-    {
-        int sphereCount = spheres.Length;
-
-        if (sphereCount == 0)
-        {
-            Debug.Log("All spheres collected! Portal opened.");
+        int pontuacaoModoInfinito;
+        GameObject[] spheres;
+        public static GameManager Instance;
             
-            
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
         
+
+        // Update is called once per frame
+        void Update()
+        {
         
-            
-
-
-
-
         }
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        sphereCount();
-        openDoor();
     }
 }
