fc509244ad1309374e2abee288c00497390566bb
DavidQ50 <davidquerido06@gmail.com>
2026-05-19 16:18:52 +0100

    limpeza

--- Pac-Mon/Assets/Scripts/GameManager.cs
+++ Pac-Mon/Assets/Scripts/GameManager.cs
@@ -1,32 +1,61 @@
 using UnityEngine;
 
-namespace game
+public class GameManager : MonoBehaviour
 {
-    public class GameManager : MonoBehaviour
+    public enum GameMode { Normal, Infinito };
+    public GameMode currentGameMode;
+    int pontuacaoModoInfinito;
+    GameObject[] spheres;
+    public static GameManager Instance;
+
+    private void Awake()
     {
-        int pontuacaoModoInfinito;
-        GameObject[] spheres;
-        public static GameManager Instance;
-            
-        private void Awake()
+        if (Instance == null)
         {
-            if (Instance == null)
-            {
-                Instance = this;
-                DontDestroyOnLoad(gameObject);
-            }
-            else
-            {
-                Destroy(gameObject);
-            }
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
-
-        
-
-        // Update is called once per frame
-        void Update()
+        else
         {
-        
+            Destroy(gameObject);
         }
+    }
+
+    void Start()
+    {
+        if (currentGameMode == GameMode.Infinito)
+        {
+            pontuacaoModoInfinito = 0;
+        }
+    }
+    
+    void sphereCount()
+    {
+        spheres = GameObject.FindGameObjectsWithTag("Spheres");
+        Debug.Log("Number of spheres: " + spheres.Length);
+    }
+
+    void openDoor()
+    {
+        int sphereCount = spheres.Length;
+
+        if (sphereCount == 0) // Example condition, adjust as needed
+        {
+            Debug.Log("All spheres collected! Door opened.");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentGameMode == GameMode.Infinito)
+        {
+           
+        }
+        else
+        {
+            sphereCount();
+            openDoor();
+        }
+        
     }
 }
